98da9c9742be533ad09da288fe00bdf293078ba3
cinsua <insua35641@gmail.com>
2018-12-12 14:52:04 -0300

    Format standarized

--- config/DB.js
+++ config/DB.js
@@ -9,23 +9,24 @@ const uriDB = `${CONFIG.MONGO.CONN_URL}/${CONFIG.MONGO.DB_NAME}`
 
 //export this function and imported by app.js
 module.exports = {
-  connectDB: function() {
+
+  connectDB: function () {
     mongoose.Promise = global.Promise
     mongoose.connect(uriDB, options);
-    mongoose.connection.on('connected', function(){
+    mongoose.connection.on('connected', function () {
       console.log(`${server.tagCyan} Succefully connected to ${uriDB}`); // as ${options.user}
     });
 
-    mongoose.connection.on('error', function(err){
+    mongoose.connection.on('error', function (err) {
       console.log(`${server.tagRed} Cannot connect to DB, reason: ${err}`);
     });
 
-    mongoose.connection.on('disconnected', function(){
+    mongoose.connection.on('disconnected', function () {
       console.log(`${server.tagRed} Disconnected from DB`);
     });
 
     process.on('SIGINT', () => {
-      if(CONFIG.MONGO.DROP_DATABASE_AT_EXIT){
+      if (CONFIG.MONGO.DROP_DATABASE_AT_EXIT) {
         console.log(`${server.tagMagenta} Deleting DB`);
         mongoose.connection.dropDatabase();
       }
@@ -34,6 +35,6 @@ module.exports = {
       console.log(`${server.tagRed} Exiting`);
       process.exit(0);
     })
-  
+
   }
 }
--- tools/serverTools.js
+++ tools/serverTools.js
@@ -1,14 +1,17 @@
 //require chalk module to give colors to console text
 var chalk = require('chalk');
+
 module.exports = {
- tagCyan: chalk.bold.cyan('[SERVER]'),
- tagYellow: chalk.bold.yellow('[SERVER]'),
- tagRed: chalk.bold.red('[SERVER]'),
- tagMagenta: chalk.bold.magenta('[SERVER]'),
- tagGreen: chalk.bold.green('[SERVER]'),
- tagWhite: chalk.bold.white('[SERVER]'),
- pad: (pad, str, padLeft) => {
-    if (typeof str === 'undefined') 
+
+  tagCyan: chalk.bold.cyan('[SERVER]'),
+  tagYellow: chalk.bold.yellow('[SERVER]'),
+  tagRed: chalk.bold.red('[SERVER]'),
+  tagMagenta: chalk.bold.magenta('[SERVER]'),
+  tagGreen: chalk.bold.green('[SERVER]'),
+  tagWhite: chalk.bold.white('[SERVER]'),
+
+  pad: (pad, str, padLeft) => {
+    if (typeof str === 'undefined')
       return pad;
     if (padLeft) {
       return (pad + str).slice(-pad.length);
@@ -16,48 +19,48 @@ module.exports = {
       return (str + pad).substring(0, pad.length);
     }
   },
-  showReq: (req, err)=>{
+
+  showReq: (req, err) => {
     let request = `[REQUEST]`
-    let method =''
+    let method = ''
     if (method === 'DELETE') method = 'DLT'
     if (method === 'PATCH') method = 'PTCH'
-    switch(req.method) {
-        case 'GET':
-          method = chalk.blue('[ GET  ]')
-          break;
-        case 'POST':
-          method = chalk.green('[ POST ]')
-          break;
-        case 'DELETE':
-          method = chalk.magenta('[DELETE]')
-          break;
-        case 'PATCH':
-          method = chalk.yellow('[PATCH ]')
-          break;
-        default:
-          method = chalk.red('[OTHER ]')
-      }
+    switch (req.method) {
+      case 'GET':
+        method = chalk.blue('[ GET  ]')
+        break;
+      case 'POST':
+        method = chalk.green('[ POST ]')
+        break;
+      case 'DELETE':
+        method = chalk.magenta('[DELETE]')
+        break;
+      case 'PATCH':
+        method = chalk.yellow('[PATCH ]')
+        break;
+      default:
+        method = chalk.red('[OTHER ]')
+    }
     let status = ''
-    if (req.status < 300){
-        status = chalk.green(`[${req.status}]`)
-    }else if(req.status < 500){
-        status = chalk.yellow(`[${req.status}]`)
-    }else{
-        status = chalk.bold.red(`[${req.status}]`)
+    if (req.status < 300) {
+      status = chalk.green(`[${req.status}]`)
+    } else if (req.status < 500) {
+      status = chalk.yellow(`[${req.status}]`)
+    } else {
+      status = chalk.bold.red(`[${req.status}]`)
     }
     let error = ' '
-    if(err){
-        error = chalk.bold.red(` [${err.code}] [${err.name}] `)
-        request = chalk.bold.red(request)
+    if (err) {
+      error = chalk.bold.red(` [${err.code}] [${err.name}] `)
+      request = chalk.bold.red(request)
     }
 
     console.log(`${request} ${method} ${status}${error}${req.originalUrl}`)
 
-
   },
-  showTrace: (errors, err)=>{
+  showTrace: (errors, err) => {
     console.log('\n')
-    for (error of errors){
+    for (error of errors) {
       errBanner = chalk.bgRed(('ERROR'.padStart(38)).padEnd(80))
       console.log(errBanner)
       console.log(`[${error.name}] [${error.code}] [${error.message}]\n`)
