ed1877b7679ebe607eb1178f41ffa4e2f14ff7ec
cinsua <insua35641@gmail.com>
2019-01-12 04:36:34 -0300

    Service Hateoas minor refactor

--- v1/services/hateoas.js
+++ v1/services/hateoas.js
@@ -5,55 +5,6 @@
 
 const utils = require('../utils/utils')
 
-module.exports = {
-
-  addLinks (result, credentials, routes, {paginationInfo} = {}) {
-    let { bestRole, route, originalUrl } = credentials
-    role = bestRole
-    let pagination = undefined
-    // should keep others query params
-    urlWithoutQuery = originalUrl.split('?')[0]
-    //let paginationInfo = options.paginationInfo
-
-    if (paginationInfo) {
-      pagination = {
-        totalPages: paginationInfo.totalPages,
-        page: paginationInfo.page,
-        totalDocs: paginationInfo.totalDocs,
-        limit: paginationInfo.limit
-      }
-      pagination.links = []
-      if (!utils.arraysEqual(result, [])) {
-        pagination.links = [{
-          type: 'GET', rel: 'first',
-          href: `${urlWithoutQuery}?page=1&limit=${paginationInfo.limit}`
-        },]
-      }
-      if (paginationInfo.hasPrevPage)
-        pagination.links.push({
-          type: 'GET', rel: 'prev',
-          href: `${urlWithoutQuery}?page=${paginationInfo.prevPage}&limit=${paginationInfo.limit}`
-        })
-      if (paginationInfo.hasNextPage)
-        pagination.links.push({
-          type: 'GET', rel: 'next',
-          href: `${urlWithoutQuery}?page=${paginationInfo.nextPage}&limit=${paginationInfo.limit}`
-        })
-
-      if (paginationInfo.totalPages > 1)
-        pagination.links.push({
-          type: 'GET', rel: 'last',
-          href: `${urlWithoutQuery}?page=${paginationInfo.pages}&limit=${paginationInfo.limit}`
-        })
-    }
-
-    data = { pagination, result }
-    links = getChilds(role, route, originalUrl, routes)
-    if (Array.isArray(links) && links.length) data.links = links
-    return data
-  }
-}
-
 function getChilds(role, route, originalUrl, routes) {
 
   // we standarize with / at the end of urls. and delete query params
@@ -63,15 +14,15 @@ function getChilds(role, route, originalUrl, routes) {
 
   // map all child routes that not have ':' after the route provided:
   // not having ':' means that is not a specific resourse
-  childRoutes = Object.keys(routes)
+  let childRoutes = Object.keys(routes)
     .filter((r) => (r !== route && r.startsWith(route) && !r.split(route)[1].includes(':')))
     .map((r) => (r.split(route)[1]))
 
-  links = []
+  let links = []
 
   // nested sh*t ... we should refactor
-  for (r of childRoutes) {
-    for (method of Object.keys(routes[route + r])) {
+  for (let r of childRoutes) {
+    for (let method of Object.keys(routes[route + r])) {
       if (routes[route + r][method].roleRequired.includes(role)) {
         links.push({
           type: method,
@@ -84,4 +35,61 @@ function getChilds(role, route, originalUrl, routes) {
 
   return links
 
+}
+
+module.exports = {
+
+  addLinks(result, credentials, routes, { paginationInfo } = {}) {
+    let { bestRole, route, originalUrl } = credentials
+    let role = bestRole
+    let pagination
+    // should keep others query params
+    let urlWithoutQuery = originalUrl.split('?')[0]
+    //let paginationInfo = options.paginationInfo
+
+    let data = { pagination, result }
+    let links = getChilds(role, route, originalUrl, routes)
+    if (Array.isArray(links) && links.length) data.links = links
+    if (!paginationInfo)
+      return data
+
+    pagination = {
+      totalPages: paginationInfo.totalPages,
+      page: paginationInfo.page,
+      totalDocs: paginationInfo.totalDocs,
+      limit: paginationInfo.limit
+    }
+    pagination.links = []
+    if (!utils.arraysEqual(result, [])) {
+      let first = {
+        type: 'GET', rel: 'first',
+        href: `${urlWithoutQuery}?page=1&limit=${paginationInfo.limit}`
+      }
+      pagination.links = [first]
+    }
+    if (paginationInfo.hasPrevPage) {
+      let prev = {
+        type: 'GET', rel: 'prev',
+        href: `${urlWithoutQuery}?page=${paginationInfo.prevPage}&limit=${paginationInfo.limit}`
+      }
+      pagination.links.push(prev)
+    }
+    if (paginationInfo.hasNextPage) {
+      let next = {
+        type: 'GET', rel: 'next',
+        href: `${urlWithoutQuery}?page=${paginationInfo.nextPage}&limit=${paginationInfo.limit}`
+      }
+      pagination.links.push(next)
+    }
+
+    if (paginationInfo.totalPages > 1) {
+      let last = {
+        type: 'GET', rel: 'last',
+        href: `${urlWithoutQuery}?page=${paginationInfo.pages}&limit=${paginationInfo.limit}`
+      }
+      pagination.links.push(last)
+    }
+
+    return data
+  }
 }
